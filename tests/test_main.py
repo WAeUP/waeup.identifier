ff40b6f7cbb7c0ff8650f53615f61c3c46fabf73
ulif <uli@gnufix.de>
2014-06-04 18:16:55 +0200

    Add infrastructure for testing.

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from waeup.identifier import main
+import waeup.identifier
+
+class MainTests(unittest.TestCase):
+
+    def test_foo(self):
+        # replace me with real tests.
+        assert main is not None
+        assert main() is None
