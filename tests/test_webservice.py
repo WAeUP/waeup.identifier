b7cdc3cec9598ff64fb67d6c7dee2bba1000e43a
ulif <uli@gnufix.de>
2014-10-10 01:45:02 +0200

    Extend tests.

--- tests/test_webservice.py
+++ tests/test_webservice.py
@@ -1,8 +1,8 @@
 import threading
 import unittest
 import xmlrpc.client
-from waeup.identifier.testing import AuthenticatingXMLRPCServer
-
+from waeup.identifier.testing import AuthenticatingXMLRPCServer, fake_student_db
+from waeup.identifier.testing import fake_student_db
 
 class WebserviceTests(unittest.TestCase):
 
@@ -12,7 +12,7 @@ class WebserviceTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # start fake server in background once for all webservice tests
-        cls.server = AuthenticatingXMLRPCServer('127.0.0.1', 61616)
+        cls.server = AuthenticatingXMLRPCServer('127.0.0.1', 61615)
         cls.server_thread = threading.Thread(
             target=cls.server.serve_forever)
         cls.server_thread.daemon = True
@@ -24,7 +24,7 @@ class WebserviceTests(unittest.TestCase):
 
     def setUp(self):
         self.proxy = xmlrpc.client.ServerProxy(
-            "http://mgr:mgrpw@localhost:61616")
+            "http://mgr:mgrpw@localhost:61615")
 
     def test_internal_ping(self):
         # make sure the fake xmlrpc server works
@@ -32,6 +32,46 @@ class WebserviceTests(unittest.TestCase):
 
     def test_internal_auth(self):
         # make sure our fake xmlrpc server requires authentication
-        proxy = xmlrpc.client.ServerProxy("http://localhost:61616")
+        proxy = xmlrpc.client.ServerProxy(
+            "http://localhost:61615")
         self.assertRaises(
             xmlrpc.client.ProtocolError, proxy.ping, 42)
+
+    def test_internal_methods(self):
+        # the following methods are available
+        assert sorted(self.proxy.system.listMethods()) == [
+            'create_student',
+            'ping', 'put_student_fingerprints',
+            'reset_student_db', 'system.listMethods',
+            'system.methodHelp', 'system.methodSignature']
+
+    def test_internal_put_student_fingerprints(self):
+        # make sure the faked method is faked properly
+        self.proxy.create_student('AB123456')
+        assert fake_student_db == {'AB123456': {}}
+        # invalid student id
+        self.assertRaises(
+            xmlrpc.client.Fault,
+            self.proxy.put_student_fingerprints, 'invalid-id')
+        # fingerprint dict not a dict
+        self.assertRaises(
+            xmlrpc.client.Fault,
+            self.proxy.put_student_fingerprints, 'AB123456', 'not-a-dict')
+        # invalid fingerprint file type
+        self.assertRaises(
+            xmlrpc.client.Fault,
+            self.proxy.put_student_fingerprints, 'AB123456', {'1': 12})
+        # invalid file format
+        self.assertRaises(
+            xmlrpc.client.Fault,
+            self.proxy.put_student_fingerprints, 'AB123456', {
+                '1': xmlrpc.client.Binary(b'not-an-fpm-file')}
+            )
+        # valid fingerprint dict
+        assert self.proxy.put_student_fingerprints(
+            'AB123456', {
+                '1': xmlrpc.client.Binary(b'FP1-faked-fpm-file')}
+            ) == True
+        # empty fingerprint dict
+        assert self.proxy.put_student_fingerprints(
+            'AB123456', {}) == False
